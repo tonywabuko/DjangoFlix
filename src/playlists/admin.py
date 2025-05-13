f43401904510f671651e4c4bffc2d938496fb6d8
tonywabuko <tonywabuko@gmail.com>
2025-05-13 14:01:08 +0300

    Update admin.py

--- src/playlists/admin.py
+++ src/playlists/admin.py
@@ -1,26 +1,22 @@
 from django.contrib import admin
-
 from tags.admin import TaggedItemInline
-
 from .models import MovieProxy, TVShowProxy, TVShowSeasonProxy, Playlist, PlaylistItem, PlaylistRelated
 
-
-class MovieProxyAdmin(admin.ModelAdmin):
+class BaseProxyAdmin(admin.ModelAdmin):
     inlines = [TaggedItemInline]
     list_display = ['title']
     fields = ['title', 'description', 'state', 'category', 'video', 'slug']
     class Meta:
-        model = MovieProxy
+        abstract = True
 
     def get_queryset(self, request):
-        return MovieProxy.objects.all()
+        return self.model.objects.all().select_related('category', 'video')
+
+class MovieProxyAdmin(BaseProxyAdmin):
+    class Meta:
+        model = MovieProxy
 
 admin.site.register(MovieProxy, MovieProxyAdmin)
-
-
-class SeasonEpisodeInline(admin.TabularInline):
-    model = PlaylistItem
-    extra = 0
 
 class TVShowSeasonProxyAdmin(admin.ModelAdmin):
     inlines = [TaggedItemInline, SeasonEpisodeInline]
@@ -29,52 +25,24 @@ class TVShowSeasonProxyAdmin(admin.ModelAdmin):
         model = TVShowSeasonProxy
     
     def get_queryset(self, request):
-        return TVShowSeasonProxy.objects.all()
+        return TVShowSeasonProxy.objects.all().select_related('parent')
 
 admin.site.register(TVShowSeasonProxy, TVShowSeasonProxyAdmin)
 
-
-class TVShowSeasonProxyInline(admin.TabularInline):
-    model = TVShowSeasonProxy
-    extra = 0
-    fields = ['order', 'title', 'state']
-
-class TVShowProxyAdmin(admin.ModelAdmin):
+class TVShowProxyAdmin(BaseProxyAdmin):
     inlines = [TaggedItemInline, TVShowSeasonProxyInline]
-    list_display = ['title']
-    fields = ['title', 'description', 'state', 'category', 'video', 'slug']
     class Meta:
         model = TVShowProxy
-    
-    def get_queryset(self, request):
-        return TVShowProxy.objects.all()
 
 admin.site.register(TVShowProxy, TVShowProxyAdmin)
 
-
-class PlaylistRelatedInline(admin.TabularInline):
-    model = PlaylistRelated
-    fk_name = 'playlist'
-    extra = 0
-
-class PlaylistItemInline(admin.TabularInline):
-    model = PlaylistItem
-    extra = 0
-
 class PlaylistAdmin(admin.ModelAdmin):
     inlines = [PlaylistRelatedInline, PlaylistItemInline, TaggedItemInline]
-    fields = [
-        'title',
-        'description',
-        'slug',
-        'state',
-        'active'
-    ]
+    fields = ['title', 'description', 'slug', 'state', 'active']
     class Meta:
         model = Playlist
 
-    
     def get_queryset(self, request):
-        return Playlist.objects.filter(type=Playlist.PlaylistTypeChoices.PLAYLIST)
+        return Playlist.objects.filter(type=Playlist.PlaylistTypeChoices.PLAYLIST).prefetch_related('items')
 
 admin.site.register(Playlist, PlaylistAdmin)
