f52bb3c3a22c7f6830bff0c3fd29b20356035c24
tonywabuko <tonywabuko@gmail.com>
2025-05-13 13:53:09 +0300

    Update models.py

--- src/tags/models.py
+++ src/tags/models.py
@@ -3,13 +3,14 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 from django.db.models.signals import pre_save
 
+# Tagged Item Manager for handling tags
 class TaggedItemManager(models.Manager):
     def unique_list(self):
         tags_set = set(self.get_queryset().values_list('tag', flat=True))
         tags_list = sorted(list(tags_set))
         return tags_list
 
-
+# Tagged Item Model
 class TaggedItem(models.Model):
     tag = models.SlugField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
@@ -22,13 +23,38 @@ class TaggedItem(models.Model):
     def slug(self):
         return self.tag
 
-    # def get_related_object(self):
-    #     Klass = self.content_type.model_class()
-    #     return Klass.objects.get(id=self.object_id)
+# Genre Model
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
+# Movie Model
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ManyToManyField(Genre)  # Linking Genre to Movies
+    description = models.TextField()
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+# Recommendation Function (based on genres)
+def recommend_movies(user):
+    # Get movies the user has watched (assuming watched_movies is a relationship)
+    watched_movies = user.watched_movies.all()
+    
+    # Get genres of watched movies
+    watched_genres = Genre.objects.filter(movie__in=watched_movies)
+    
+    # Recommend movies of the same genres, excluding already watched movies
+    recommended_movies = Movie.objects.filter(genre__in=watched_genres).exclude(id__in=watched_movies)
+    
+    return recommended_movies
+
+# Signal for lowercase tags
 def lowercase_tag_pre_save(sender, instance, *args, **kwargs):
     instance.tag = f"{instance.tag}".lower()
-
 
 pre_save.connect(lowercase_tag_pre_save, sender=TaggedItem)
