c3dcf50f8f0a723215e0d2c9e0f6f098c5929e94
tonywabuko <tonywabuko@gmail.com>
2025-05-13 14:02:42 +0300

    Update test_tv_shows.py

--- src/playlists/test_tv_shows.py
+++ src/playlists/test_tv_shows.py
@@ -1,42 +1,35 @@
 from django.test import TestCase
-
 from django.utils import timezone
 from django.utils.text import slugify
-
 from djangoflix.db.models import PublishStateOptions
-
 from videos.models import Video
 from .models import TVShowProxy, TVShowSeasonProxy
 
 class TVShowProxyModelTestCase(TestCase):
-    def create_show_with_seasons(self):
-        the_office = TVShowProxy.objects.create(title='The Office Series')
-        self.season_1 = TVShowSeasonProxy.objects.create(title='The Office Series Season 1', state=PublishStateOptions.PUBLISH, parent=the_office, order=1)
-        season_2 = TVShowSeasonProxy.objects.create(title='The Office Series Season 2', parent=the_office, order=2)
-        season_3 = TVShowSeasonProxy.objects.create(title='The Office Series Season 3', parent=the_office, order=3)
-        season_4 = TVShowSeasonProxy.objects.create(title='The Office Series Season 4', parent=the_office, order=4)
-        self.season_11 = TVShowSeasonProxy.objects.create(title='The Office Series Season 1', parent=the_office, order=4)
-        self.show = the_office
 
-    def create_videos(self):
+    def setUp(self):
+        # Create videos
         video_a = Video.objects.create(title='My title', video_id='abc123')
         video_b = Video.objects.create(title='My title', video_id='abc1233')
         video_c = Video.objects.create(title='My title', video_id='abc1234')
         self.video_a = video_a
         self.video_b = video_b
         self.video_c = video_c
-        self.video_qs = Video.objects.all()
 
+        # Create show with seasons
+        the_office = TVShowProxy.objects.create(title='The Office Series')
+        self.season_1 = TVShowSeasonProxy.objects.create(title='The Office Series Season 1', state=PublishStateOptions.PUBLISH, parent=the_office, order=1)
+        self.season_2 = TVShowSeasonProxy.objects.create(title='The Office Series Season 2', parent=the_office, order=2)
+        self.season_3 = TVShowSeasonProxy.objects.create(title='The Office Series Season 3', parent=the_office, order=3)
+        self.season_4 = TVShowSeasonProxy.objects.create(title='The Office Series Season 4', parent=the_office, order=4)
+        self.season_11 = TVShowSeasonProxy.objects.create(title='The Office Series Season 1', parent=the_office, order=4)
+        self.show = the_office
 
-    def setUp(self):
-        self.create_videos()
-        self.create_show_with_seasons()
+        # Create TV shows with videos
         self.obj_a = TVShowProxy.objects.create(title='This is my title', video=self.video_a)
-        obj_b = TVShowProxy.objects.create(title='This is my title', state=PublishStateOptions.PUBLISH, video=self.video_a)
-        # obj_b.videos.set([self.video_a, self.video_b, self.video_c])
-        obj_b.videos.set(self.video_qs)
-        obj_b.save()
-        self.obj_b = obj_b
+        self.obj_b = TVShowProxy.objects.create(title='This is my title', state=PublishStateOptions.PUBLISH, video=self.video_a)
+        self.obj_b.videos.set([self.video_a, self.video_b, self.video_c])
+        self.obj_b.save()
 
     def test_show_has_seasons(self):
         seasons = self.show.playlist_set.all()
@@ -54,16 +47,14 @@ class TVShowProxyModelTestCase(TestCase):
         self.assertEqual(count, 3)
 
     def test_playlist_video_through_model(self):
-        v_qs = sorted(list(self.video_qs.values_list('id')))
         video_qs = sorted(list(self.obj_b.videos.all().values_list('id')))
         playlist_item_qs = sorted(list(self.obj_b.playlistitem_set.all().values_list('video')))
-        self.assertEqual(v_qs, video_qs, playlist_item_qs)
+        self.assertEqual(video_qs, playlist_item_qs)
 
-
-    def test_video_playlist_ids_propery(self):
+    def test_video_playlist_ids_property(self):
         ids = self.obj_a.video.get_playlist_ids()
-        acutal_ids = list(TVShowProxy.objects.all().filter(video=self.video_a).values_list('id', flat=True))
-        self.assertEqual(ids, acutal_ids)
+        actual_ids = list(TVShowProxy.objects.all().filter(video=self.video_a).values_list('id', flat=True))
+        self.assertEqual(ids, actual_ids)
 
     def test_video_playlist(self):
         qs = self.video_a.playlist_featured.all()
@@ -73,9 +64,9 @@ class TVShowProxyModelTestCase(TestCase):
         title = self.obj_a.title
         test_slug = slugify(title)
         self.assertEqual(test_slug, self.obj_a.slug)
-    
+
     def test_valid_title(self):
-        title='This is my title'
+        title = 'This is my title'
         qs = TVShowProxy.objects.all().filter(title=title)
         self.assertTrue(qs.exists())
 
@@ -86,7 +77,7 @@ class TVShowProxyModelTestCase(TestCase):
     def test_seasons_created_count(self):
         qs = TVShowSeasonProxy.objects.all()
         self.assertEqual(qs.count(), 5)
-    
+
     def test_tv_show_draft_case(self):
         qs = TVShowProxy.objects.all().filter(state=PublishStateOptions.DRAFT)
         self.assertEqual(qs.count(), 2)
@@ -98,15 +89,12 @@ class TVShowProxyModelTestCase(TestCase):
     def test_publish_case(self):
         qs = TVShowProxy.objects.all().filter(state=PublishStateOptions.PUBLISH)
         now = timezone.now()
-        published_qs = TVShowProxy.objects.all().filter(
-            state=PublishStateOptions.PUBLISH,
-            publish_timestamp__lte= now 
-        )
+        published_qs = TVShowProxy.objects.all().filter(state=PublishStateOptions.PUBLISH, publish_timestamp__lte=now)
         self.assertTrue(published_qs.exists())
 
     def test_publish_manager(self):
         published_qs = TVShowProxy.objects.all().published()
-        published_qs_2 = TVShowProxy.objects.all().published()
         self.assertTrue(published_qs.exists())
-        self.assertEqual(published_qs.count(), published_qs_2.count())
+        self.assertEqual(published_qs.count(), TVShowProxy.objects.all().published().count())
+
         
